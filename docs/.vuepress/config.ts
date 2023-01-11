c33bc33caf18df041f40225e96b2cf2e02e7f46d
NWUzmed <78712654+nwuzmedoutlook@users.noreply.github.com>
2023-01-11 14:30:48 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -24,7 +24,8 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       description: 'CO2等技术文章。',
     }
   },
-  // base: '/newdac/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
+  base: '/', 
+  // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
 
   // 主题配置
   themeConfig: {
