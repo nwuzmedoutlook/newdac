acbe9fc5a5ea3718cbd8a35e523fa60c28d082b5
NWUzmed <78712654+nwuzmedoutlook@users.noreply.github.com>
2023-01-15 20:27:58 +0800

    Update BlockToggle.vue

--- docs/.vuepress/components/BlockToggle.vue
+++ docs/.vuepress/components/BlockToggle.vue
@@ -185,7 +185,7 @@ div[class*="language-"]::before {
   left: 4.7rem;
   font-size: 1.15em;
   color: rgba(238, 255, 255, 0.8);
-  text-transform: uppercase;
+//  text-transform: uppercase;  //取消代码块语言大写
   font-weight: bold;
   width: fit-content;
 }
