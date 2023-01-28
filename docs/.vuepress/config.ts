e1f57d7b638a5703a0eed9a9e3d2c02d9c340a38
NWUzmed <78712654+nwuzmedoutlook@users.noreply.github.com>
2023-01-28 17:19:27 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -325,7 +325,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     
     
     // 全文搜索。 ⚠️注意：此插件会在打开网站时多加载部分js文件用于搜索，导致初次访问网站变慢。如在意初次访问速度的话可以不使用此插件！（推荐：vuepress-plugin-thirdparty-search）
-    // 'fulltext-search',
+    'fulltext-search',
 
     // 可以添加第三方搜索链接的搜索框（继承原官方搜索框的配置参数）
     [
@@ -333,21 +333,21 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       {
         thirdparty: [
           {
-            title: '在MDN中搜索',
-            frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
+            title: '在Bing中搜索',
+            frontUrl: 'https://cn.bing.com/search?q=', // 搜索链接的前面部分
             behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
           },
           {
-            title: '在Runoob中搜索',
-            frontUrl: 'https://www.runoob.com/?s=',
+            title: '在百度中搜索',
+            frontUrl: 'https://www.baidu.com/s?wd=',
           },
           {
-            title: '在Vue API中搜索',
-            frontUrl: 'https://cn.vuejs.org/v2/api/#',
+            title: '在搜狗中搜索',
+            frontUrl: 'https://www.sogou.com/web?query=',
           },
           {
-            title: '在Bing中搜索',
-            frontUrl: 'https://cn.bing.com/search?q=',
+            title: '在Google中搜索',
+            frontUrl: 'https://jproxy.revth.com/-----https://ipv6.google.com/search?q=',
           },
           {
             title: '通过百度搜索本站的',
