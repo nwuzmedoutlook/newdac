69f3830bdda06c2f01884e79e8f66521e1c40f04
NWUzmed <78712654+nwuzmedoutlook@users.noreply.github.com>
2023-01-28 19:28:17 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -325,9 +325,9 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     
     
     // 全文搜索。 ⚠️注意：此插件会在打开网站时多加载部分js文件用于搜索，导致初次访问网站变慢。如在意初次访问速度的话可以不使用此插件！（推荐：vuepress-plugin-thirdparty-search）
-    'fulltext-search',
+    // 'fulltext-search',
 
-    // 可以添加第三方搜索链接的搜索框（继承原官方搜索框的配置参数）
+    // 可以添加第三方搜索链接的搜索框（继承原官方搜索框的配置参数，即最多只能搜索到三级标题，无法搜索正文），该插件不能与fulltext-search插件同时开启，否则仅显示该插件样式
     [
       'thirdparty-search',
       {
